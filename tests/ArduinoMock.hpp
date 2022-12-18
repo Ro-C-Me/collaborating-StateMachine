a2d95abb2b8673919142484d9928873db546f6e8
Robert Meyer <rme@hacon.de>
2022-12-18 18:33:40 +0100

    refactoring: moved test stuff in own folder

---
+++ tests/ArduinoMock.hpp
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <iostream>
+
+const int LOW = 0;
+const int HIGH = 1;
+const int INPUT = 2;
+const int OUTPUT = 3;
+
+
+static unsigned long millis() {
+	 std::chrono::time_point<std::chrono::system_clock> now =
+	     std::chrono::system_clock::now();
+	 auto duration = now.time_since_epoch();
+	 auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+	 return long(millis);
+}
+
+static void digitalWrite(int pin, int val) {
+	std::string s;
+
+	if (val == LOW) {
+		s = "LOW";
+	}
+	else if (val == HIGH) {
+		s = "HIGH";
+	}
+	else {
+		s = "INVALID VALUE";
+	}
+	std::cout << std::string("digitalWrite: pin " + std::to_string(pin) + " -> " + s + "\n");
+}
+
+static void pinMode(int pin, int mode) {
+	std::string s;
+
+	if (mode == INPUT) {
+		s = "INPUT";
+	}
+	else if (mode == OUTPUT) {
+		s = "OUTPUT";
+	}
+	else {
+		s = "INVALID VALUE";
+	}
+	std::cout << std::string("pinMode: pin " + std::to_string(pin) + " -> " + s + "\n");
+}
