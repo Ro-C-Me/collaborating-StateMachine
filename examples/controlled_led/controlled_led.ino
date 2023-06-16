12d3a21204031acb90e20d555d3c404791688a3c
Robert Meyer <rme@hacon.de>
2023-06-16 23:49:07 +0200

    a second example: controlled LED

---
+++ examples/controlled_led/controlled_led.ino
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include "StateMachine.hpp"
+
+class LED: public StateMachine {
+
+private:
+
+	static const int SWITCH_ON = 1;
+	static const int ON = 2;
+	static const int SWITCH_OFF = 3;
+	static const int OFF = 4;
+
+	int pin;
+
+public:
+	LED(int pin) {
+		this->pin = pin;
+		this->state = ON;
+	}
+
+	void init() override {
+		digitalWrite(pin, LOW);
+		pinMode(pin, OUTPUT);
+	}
+
+	void step() override {
+		if (this->state == SWITCH_ON) {
+			this->state = ON;
+			digitalWrite(this->pin, HIGH);
+			this->wait(1000, SWITCH_OFF);
+		}
+		else if (this->state == SWITCH_OFF) {
+			this->state = OFF;
+			digitalWrite(this->pin, LOW);
+		}
+	}
+
+	bool isAvailable() override {
+		return this->state == OFF;
+	}
+
+	void switchOn() {
+		this->state = SWITCH_ON;
+	}
+};
+
+class LEDControl : StateMachine {
+
+private:
+	static const int SWITCH_LED_ON = 0;
+	static const int IDLE = 1;
+
+	LED* led;
+
+public:
+	LEDControl(LED* led) {
+		this->led = led;
+	}
+
+	void step() override {
+		if (this->state == SWITCH_LED_ON) {
+			this->led->switchOn();
+			this->waitFor(this->led, IDLE);
+		}
+		else if (this->state == IDLE) {
+			this->wait(2000, SWITCH_LED_ON);
+		}
+	}
+};
+
+void setup() {
+	LED led(13);
+	new LEDControl(&led);
+
+	StateMachine::registry->init();
+}
+
+void loop() {
+	StateMachine::registry->nextStep();
+}
