f5ba46d736b74428c747ab9ac28ca963637fd4c2
Robert Meyer <rme@hacon.de>
2023-06-17 22:16:40 +0200

    use two leds

--- examples/blinking_led/blinking_led.ino
+++ examples/blinking_led/blinking_led.ino
@@ -8,35 +8,50 @@ private:
 	static const int ON = 0;
 	static const int OFF = 1;
 
+	// PIN used by the LED
 	int pin;
 
+	// time in ms to led the LED on / off
+	int blinkingTime;
+
 public:
-	LED(int pin) {
+	LED(int pin, int blinkingTime) {
 		this->pin = pin;
+		this->blinkingTime = blinkingTime;
 		this->state = ON;
 	}
 
+	// do what we normally do in setup(): setting PIN modes etc.
 	void init() override {
 		digitalWrite(pin, LOW);
 		pinMode(pin, OUTPUT);
 	}
 
+	// do the magic
 	void step() override {
+
+		// if state is ON: light up and wait some time :)
 		if (this->state == ON) {
 			digitalWrite(this->pin, HIGH);
-			this->wait(1000, OFF);
+			this->wait(this->blinkingTime, OFF);
 		} else {
+			// otherwise: set light off and wait again!
 			digitalWrite(this->pin, LOW);
-			this->wait(2000, ON);
+			this->wait(blinkingTime, ON);
 		}
 	}
 };
 
+//define our leds
+LED led1(13, 300);
+LED led2(14, 1000);
+
+// initialize all known stateMachines
 void setup() {
-	new LED(13);
 	StateMachine::registry->init();
 }
 
 void loop() {
+	// let the magic start
 	StateMachine::registry->nextStep();
 }
